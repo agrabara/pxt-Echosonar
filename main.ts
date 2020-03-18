7c8d21d71230f0c1cd19eac9f3e0a69a3a0b214a
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-03-18 22:42:15 +0100

    Drobne zmiany

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 let distance = 0
 basic.showIcon(IconNames.Happy)
 I2C_LCD1602.LcdInit(63)
+I2C_LCD1602.BacklightOff()
+I2C_LCD1602.ShowString("Hello!", 0, 0)
 I2C_LCD1602.BacklightOn()
-I2C_LCD1602.ShowString("Hello", 0, 0)
+basic.pause(1000)
+I2C_LCD1602.ShowString("Sonar    ", 0, 0)
 basic.pause(1000)
 I2C_LCD1602.clear()
 basic.showLeds(`
@@ -18,9 +21,9 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P0, 0)
+    //distance = Math.idiv(pins.pulseIn(DigitalPin.P1, PulseValue.High, 300 * 58), 58)
     distance = Math.idiv(pins.pulseIn(DigitalPin.P1, PulseValue.High), 58)
     basic.pause(300)
-    I2C_LCD1602.clear()
-    I2C_LCD1602.ShowNumber(distance, 0, 0)
+    I2C_LCD1602.ShowString("Distance:       ", 0, 0)
+    I2C_LCD1602.ShowNumber(distance, 9, 0)
 })
- 
